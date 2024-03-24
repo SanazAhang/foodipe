7701c588e7d7d7192262d5fc929ac3527302527d
sanaz <sanazramezanpur6@gmail.com>
2024-03-24 10:15:02 +0330

    Base structure of project

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "foodipe"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(":app")
+include(":build-logic")
+include(":core:ui")
+include(":core:data")
+include(":core:domain")
+include(":core:network")
+include(":core:database")
+include(":core:common")
